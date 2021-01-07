5f0ed4bd823d32ed45d9c4590dd08397d8ae2c10
Michal S <michalsienczuk@gmail.com>
2021-01-07 22:34:04 +0100

    task 10.4 Subpages support

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,63 @@
-import {select, settings,} from './settings.js';
+import {select, settings, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 
 const app = {
+  initBooking: function (){
+    const thisApp = this;
+    thisApp.bookingWrapper = document.querySelector(select.containerOf.booking);
+    new Booking(thisApp.bookingWrapper);
+  },
+  initPages: function (){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function (event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        // get page id from href attribute
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        // run thisApp.activePage with that id
+        thisApp.activatePage(id);
+        // change URL hash
+        window.location.hash = '#/' + id;
+      });
+    }
+
+  },
+  activatePage: function (pageId){
+    const thisApp = this;
+
+    // add class "active" to matching pages, remove from non-matching
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    // add class "active" to matching links, remove from non-matching
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId);
+    }
+
+
+  },
   initData: function () {
     const thisApp = this;
     thisApp.data = {};
@@ -40,6 +94,9 @@ const app = {
 
   init: function () {
     const thisApp = this;
+    thisApp.initBooking();
+
+    thisApp.initPages();
 
     thisApp.initData();
     // thisApp.initMenu();
